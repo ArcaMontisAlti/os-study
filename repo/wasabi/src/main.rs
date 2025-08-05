26e4fc1bb416b6ab71a86ce240b47462663d791e
Noa Takayama <nailinggaoshan4@gmail.com>
2025-08-05 23:57:39 +0900

    とりあえず、画面を白くすることはできた

--- repo/wasabi/src/main.rs
+++ repo/wasabi/src/main.rs
@@ -1,14 +1,125 @@
-#![no_std] // 標準ライブラリを使わないよ, ということを宣言する
-#![no_main] // main関数は使わないよ, ということを宣言する
+#! [no_std] // no_std を使うための宣言
+#! [no_main] // no_main を使うための宣言
+#! [feature(offset_of)]
+// core クレート内にある型を使うために use 宣言を行う
+use core::mem::offset_of;
+use core::mem::size_of;
+use core::panic::PanicInfo;
+use core::ptr::null_mut;
+use core::slice;
+
+type EfiVoid = u8;
+type EfiHandle = u64;
+type Result<T> = core::result::Result<T, &'static str>;
+
+// EfiGuid の実装部分
+#[repr(C)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct EfiGuid {
+    pub data0: u32,
+    pub data1: u16,
+    pub data2: u16,
+    pub data3: [u8; 8],
+}
+
+const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EfiGuid = EfiGuid {
+    data0: 0x9042a9de,
+    data1: 0x23dc,
+    data2: 0x4a38,
+    data3: [0x96, 0xfb, 0x7a, 0xde, 0xd0, 0x80, 0x51, 0x6a],
+};
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+#[must_use]
+#[repr(u64)]
+enum EfiStatus {
+    Success = 0,
+}
+
+#[repr(C)]
+struct EfiBootServicesTable {
+    _reserved0: [u64; 40],
+    locate_protocol: extern "win64" fn(
+        protocol: *const EfiGuid,
+        registration: *const EfiVoid,
+        interface: *mut *mut EfiVoid,
+    ) -> EfiStatus,
+}
+const _: () = assert!(offset_of!(EfiBootServicesTable, locate_protocol) == 320);
+
+// EfiGraphicsProtocol の中の mode というメンバの実装
+#[repr(C)]
+struct EfiSystemTable {
+    _reserved0: [u64; 12],
+    pub boot_services: &'static EfiBootServicesTable,
+}
+const _: () = assert!(offset_of!(EfiSystemTable, boot_services) == 96); 
+
+// メンバ info を辿ることで, より詳しい画面モードを取得するための構造体の実装
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocolPixelInfo {
+    version: u32,
+    pub horizontal_resolution: u32, // 水平方向の画素数. u32 というのは, 32bit の符号なし整数
+    pub vertical_resolution: u32, // 垂直方向の画素数
+    _padding0: [u32; 5],
+    pub pixels_per_scan_line: u32, // 水平方向のデータに含まれる画素数. horizontal_resolution と同じ値になるとは限らないため必要
+}
+const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
+
+// mode が指す先の構造体の実装
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocolMode<'a> {
+    pub max_mode: u32,
+    pub mode: u32,
+    pub info: &'a EfiGraphicsOutputProtocolPixelInfo,
+    pub size_of_info: u64,
+    pub frame_buffer_base: usize,
+    pub frame_buffer_size: usize,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocol<'a> {
+    reserved: [u64; 3],
+    pub mode: &'a EfiGraphicsOutputProtocolMode<'a>,
+}
+
+fn locate_graphic_protocol<'a>(
+    efi_system_table: &EfiSystemTable,
+) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
+    let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
+    let status = (efi_system_table.boot_services.locate_protocol)(
+        &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
+        null_mut::<EfiVoid>(),
+        &mut graphic_output_protocol as *mut *mut EfiGraphicsOutputProtocol
+            as *mut *mut EfiVoid,
+    );
+    if status != EfiStatus::Success {
+        return Err("Failed to locate graphics output protocol");
+    }
+    Ok(unsafe { &*graphic_output_protocol })
+}
 
 #[no_mangle] // p.56 より, これをつけることで、コンパイラが関数名を変更しないようにする
-
-fn efi_main() {
+fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
+    let efi_graphics_output_protocol =
+        locate_graphic_protocol(efi_system_table).unwrap();
+    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
+    let vram = unsafe {
+        slice::from_raw_parts_mut(
+            vram_addr as *mut u32, 
+            vram_byte_size / size_of::<u32>(),
+        )
+    };
+    for e in vram {
+        *e = 0xffffff; // 白色で画面を塗りつぶす
+    }
     // println!("Hello, world!");
     loop {}
 }
-
-use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
