eb3e65e02f4769c884b92e565ff03bf1027846bf
Noa Takayama <nailinggaoshan4@gmail.com>
2025-08-05 20:53:42 +0900

    Next page 59, writing something to flamebuffer

--- repo/wasabi/src/main.rs
+++ repo/wasabi/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // 標準ライブラリを使わないよ, ということを宣言する
+#![no_main] // main関数は使わないよ, ということを宣言する
+
+#[no_mangle] // p.56 より, これをつけることで、コンパイラが関数名を変更しないようにする
+
+fn efi_main() {
+    // println!("Hello, world!");
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
